1d50a51fc4c7f754b4fcbcdfd3c965225198db84
Calvin Chestnut <calvin_chestnut@me.com>
2019-06-06 07:40:47 -0400

    Migrate TextEditor into new package

--- Package.swift
+++ Package.swift
@@ -6,23 +6,15 @@ import PackageDescription
 let package = Package(
     name: "TextEditor",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "TextEditor",
             targets: ["TextEditor"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "TextEditor",
             dependencies: []),
-        .testTarget(
-            name: "TextEditorTests",
-            dependencies: ["TextEditor"]),
     ]
 )
--- Sources/TextEditor/TextEditor.swift
+++ Sources/TextEditor/TextEditor.swift
@@ -1,3 +1,108 @@
-struct TextEditor {
-    var text = "Hello, World!"
+import SwiftUI
+import UIKit
+
+@available(iOS 13.0, *)
+struct TextEditor: View {
+    @Binding var text: String
+    
+    var body: some View {
+        TextField_UI(text: $text)
+    }
 }
+
+@available(iOS 13.0, *)
+extension Font.TextStyle {
+    static var allValues: [Font.TextStyle: Font] {
+        return [
+            .largeTitle: .largeTitle,
+            .title: .title,
+            .headline: .headline,
+            .subheadline: .subheadline,
+            .body: .body,
+            .callout: .callout,
+            .footnote: .footnote,
+            .caption: .caption
+        ]
+    }
+    var style_ui: UIFont.TextStyle {
+        switch self {
+        case .largeTitle: return .largeTitle
+        case .title: return .title1
+        case .headline: return .headline
+        case .subheadline: return .subheadline
+        case .callout: return .callout
+        case .footnote: return .footnote
+        case .caption: return .caption1
+        default: return .body
+        }
+    }
+}
+@available(iOS 13.0, *)
+extension Font {
+    var font_ui: UIFont {
+        return UIFont.preferredFont(forTextStyle: assertStyle().style_ui)
+    }
+    private func assertStyle(_ fallback: TextStyle = .body) -> TextStyle {
+        guard let key = TextStyle.allValues.filter({ self == $0.value }).map({ $0.key }).first else { return fallback }
+        return key
+    }
+}
+
+@available(iOS 13.0, *)
+struct TextField_UI : UIViewRepresentable {
+    typealias UIViewType = UITextView
+    
+    @Binding var text: String
+    @State var font: Font = .body
+    var onEditingChanged: ((String) -> Void)?
+    var onCommit: (() -> Void)?
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> UITextView {
+        let textView = UITextView()
+        textView.backgroundColor = nil
+        textView.text = text
+        if let font = context.environment.font {
+            textView.font = font.font_ui
+        } else {
+            textView.font = .preferredFont(forTextStyle: .body)
+        }
+        textView.textContainerInset = .zero
+        
+        textView.delegate = context.coordinator
+        
+        return textView
+    }
+    
+    func updateUIView(_ textView: UITextView, context: Context) {
+        if let font = context.environment.font, font.font_ui != textView.font {
+            textView.font = font.font_ui
+        }
+        textView.text = text
+    }
+    
+    class Coordinator: NSObject, UITextViewDelegate {
+        var field: TextField_UI
+        
+        init(_ field: TextField_UI) {
+            self.field = field
+        }
+        
+        func textViewDidChange(_ textView: UITextView) {
+            print(textView.text ?? "Nil")
+        }
+    }
+}
+
+#if DEBUG
+@available(iOS 13.0, *)
+struct TextEditor_Previews : PreviewProvider {
+    static var previews: some View {
+        TextEditor(text: .constant("Hello TextField"))
+            .font(.largeTitle)
+    }
+}
+#endif
